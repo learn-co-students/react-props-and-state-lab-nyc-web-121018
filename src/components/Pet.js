2cbd2e9da8cae1ef59dea529d4596bd835413b48
diaaanek <dkorongy@gmail.com>
2019-02-26 16:29:57 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,4 +1,10 @@
 import React from 'react'
+// Should receive a pet prop. Use the attributes in this data to render the pet card correctly. It should show the pet's name, type, age and weight. Based on the pet's gender, the component also needs to contain either a male (♂) or female (♀) symbol.
+//
+//
+// Should receive an isAdopted prop. Using this prop, render the correct button in the pet's card; if the pet is adopted, show the disabled button. Otherwise, show the primary button to adopt the pet.
+//
+// Should receive an onAdoptPet callback prop. This callback prop gets called with the pet's id when the user clicks the adopt pet button — not when they click the disabled button!
 
 class Pet extends React.Component {
   render() {
@@ -6,20 +12,27 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+
+            {this.props.pet.name}{' '}
+            {this.props.pet.gender === "female" ? '♀' : '♂' }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.pet.isAdopted ? (
+          <button className="ui disabled button">Already adopted</button> ) : (
+          <button
+            onClick={()=> this.props.onAdoptPet(this.props.pet.id)}
+            className="ui primary button">
+            Adopt pet
+          </button>
+        )}
         </div>
       </div>
     )
